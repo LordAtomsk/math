2189cf28bebca706c1cc313143b388b9cc5684aa
omar <omarc4702@gmail.com>
2021-11-29 19:19:00 -0500

    my first javascript and python math

---
+++ jsMath.js
@@ -0,0 +1,10 @@
+let x = 1;
+let y = 2;
+let add = x + y;
+let sub = x - y;
+let multi = x * y;
+let divide = x/y
+console.log(add);
+console.log(sub);
+console.log(multi);
+console.log(divide);
---
+++ python_math.py
@@ -0,0 +1,10 @@
+x = 1
+y = 2
+add = x + y
+sub = x - y
+multiply = x * y
+divide = x/y
+print(add)
+print(sub)
+print(multiply)
+print(divide)
